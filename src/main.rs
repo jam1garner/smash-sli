ce891f2a0379f9f69729b40ecb5467c9236340ab
jam1garner <8260240+jam1garner@users.noreply.github.com>
2020-11-03 20:31:56 -0500

    Fix CLI

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bgm_property::BgmPropertyFile;
+use sli::SliFile;
 use structopt::StructOpt;
 
 use std::path::{Path, PathBuf};
@@ -16,19 +16,19 @@ struct Args {
 fn main() {
     let args = Args::from_args();
 
-    match BgmPropertyFile::open(&args.in_file) {
+    match SliFile::open(&args.in_file) {
         Ok(bgm_prop_file) => {
-            let _ = bgm_property::set_labels(
+            let _ = sli::set_labels(
                 args.labels.as_deref().unwrap_or(Path::new("bgm_hashes.txt"))
             );
 
             fs::write(&args.out_file, serde_yaml::to_string(&bgm_prop_file).unwrap()).unwrap();
         }
-        Err(bgm_property::Error::BadMagic { .. }) => {
+        Err(sli::Error::BadMagic { .. }) => {
             // Magic doesn't match, should be yaml file
 
             let contents = fs::read_to_string(&args.in_file).unwrap();
-            let bgm_prop_file: BgmPropertyFile = serde_yaml::from_str(&contents).unwrap();
+            let bgm_prop_file: SliFile = serde_yaml::from_str(&contents).unwrap();
 
             bgm_prop_file.save(&args.out_file).unwrap();
         },
